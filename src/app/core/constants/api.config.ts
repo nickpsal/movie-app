6bc58100fea1c6a5183e376599939f8b9e4f0bb0
NIKOLAOS PSALTAKIS <nickpsal@gmail.com>
2025-05-09 18:13:04 +0300

    fix constants file

---
+++ src/app/core/constants/api.config.ts
@@ -0,0 +1,6 @@
+export const TMDB_API_CONFIG = {
+  baseUrl: 'https://api.themoviedb.org/3',
+  apiKey: 'YOUR_TMDB_API_KEY_HERE',
+  language: 'el-GR', // or 'en-US'
+  imageBaseUrl: 'https://image.tmdb.org/t/p/w500'
+};
